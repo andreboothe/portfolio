c01edc42b310d45efa2c2f9096ff5a8ffa633795
Andre <boothe.andre@live.com>
2018-05-31 10:29:31 -0500

    added landing page

---
+++ src/components/pagesComponent/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const LandingPage = () => {
+
+    return (
+
+        <section id="landingpage" >
+            
+            <div className="white-text">
+                <h1>My passion for <span className="teal"> problem solving,</span><br />
+                lead me down the path of a <span className="teal-bottom-border">Developer. </span></h1>
+            </div>
+        </section>
+    );
+}
+
+export default LandingPage;
